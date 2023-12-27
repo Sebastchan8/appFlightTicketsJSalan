d08b3c43aadf46cf81e73b723ad6ab1a2fc44ed6
sebastchan <jsalan5133@uta.edu.ec>
2023-12-26 23:55:35 -0500

    user functions added

--- src/app/components/notification/notification.component.ts
+++ src/app/components/notification/notification.component.ts
@@ -1,20 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { FlightsService } from 'src/app/services/flights.service';
+import { ModalController } from '@ionic/angular'; // Asegúrate de importar el ModalController
 
 @Component({
   selector: 'app-notification',
   templateUrl: './notification.component.html',
   styleUrls: ['./notification.component.scss'],
 })
-export class NotificationComponent  implements OnInit {
+export class NotificationComponent implements OnInit {
 
-  notifications!:any
-  constructor(private flightsService:FlightsService) { }
+  notifications!: any;
+
+  constructor(
+    private flightsService: FlightsService,
+    private router: Router,
+    private modalController: ModalController
+  ) { }
 
   ngOnInit() {
     this.flightsService.getNotifications().subscribe(data => {
-      this.notifications = data
-    })
+      this.notifications = data;
+    });
+  }
+
+  async closeModal() {
+    await this.modalController.dismiss();
+  }
+
+  navigateToMytickets() {
+    this.router.navigate(['/mytickets']);
+    this.closeModal();
   }
 
 }
