dae44a5416e812feae3b2d0d16f28a5193276316
Sebastchan8 <jsalan5133@uta.edu.ec>
2024-06-03 20:49:02 -0500

    Discount codes added.

--- src/app/pages/checkout/checkout.page.ts
+++ src/app/pages/checkout/checkout.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { LoadingController, ModalController } from '@ionic/angular';
+import { LoadingController, ModalController, AlertController } from '@ionic/angular';
 import { SuccessModalComponent } from 'src/app/components/success-modal/success-modal.component';
 import { FlightsService } from 'src/app/services/flights.service';
 
@@ -18,12 +18,15 @@ export class CheckoutPage implements OnInit {
   round: boolean = false
   fullname!: string
   creditCard!: string
+  discountCode: string = ''; // Nuevo campo para el código de descuento
+  discountApplied: number = 0; // Nuevo campo para almacenar el descuento aplicado
 
   constructor(private flightsService: FlightsService,
     private route: ActivatedRoute,
     public loadingController: LoadingController,
     private modalController: ModalController,
-    private router: Router) { }
+    private router: Router,
+    private alertController: AlertController) { }
 
   ngOnInit() {
     let departure_id = this.route.snapshot.paramMap.get('departure_id') as string
@@ -54,6 +57,25 @@ export class CheckoutPage implements OnInit {
     return "XXXX - XXXX - XXXX - XX" + card.substring(14, 16)
   }
 
+  applyDiscount() {
+    var empresas_vuelos = [
+      {"nombre": "Ecuador Green Travel", "codigo": "EGT123", "descuento":"5"},
+      {"nombre": "Polimundo SA", "codigo": "PMS456", "descuento":"6"},
+      {"nombre": "PROVITUR ", "codigo": "PVT789", "descuento":"6"},
+      {"nombre": "Destino Ecuador", "codigo": "DEC234", "descuento":"5"},
+      {"nombre": "Cite Tour", "codigo": "CTU567", "descuento":"8"}
+  ]
+    const discountInfo = empresas_vuelos.find(emp => emp.codigo === this.discountCode);
+    if (discountInfo) {
+      this.discountApplied = parseFloat(discountInfo.descuento);
+    } else {
+      this.discountApplied = 0;
+      this.showAlert('Invalid discount code.');
+    }
+  }
+  getDiscount() {
+    return (this.getSubtotal() * this.discountApplied / 100).toFixed(2);
+  }
   getAdultSubtotal() {
     const departurePrice = parseFloat(this.departureFlight.adult_price);
 
@@ -65,7 +87,6 @@ export class CheckoutPage implements OnInit {
     return this.adults * departurePrice;
   }
 
-
   getChildSubtotal() {
     const departureChildPrice = parseFloat(this.departureFlight.child_price);
 
@@ -76,21 +97,24 @@ export class CheckoutPage implements OnInit {
 
     return this.children * departureChildPrice;
   }
-
-
   getSubtotal() {
     const sub = this.getAdultSubtotal() + this.getChildSubtotal()
     return Number(sub.toFixed(2));
   }
+  getSubtotalWithDiscount() {
+    const subtotal = this.getAdultSubtotal() + this.getChildSubtotal()
+    return (subtotal - (subtotal * (this.discountApplied / 100)));
+  }
 
   getTax() {
-    const tax = this.getSubtotal() * 0.12;
+    const tax = Number(this.getSubtotalWithDiscount()) * 0.15;
     return Number(tax.toFixed(2));
   }
 
-  getTotal() {
-    const total = this.getSubtotal() + this.getTax();
-    return Number(total.toFixed(2));
+  getTotalWithDiscount() {
+    const subtotalWithDiscount = this.getSubtotalWithDiscount();
+    const tax = subtotalWithDiscount * 0.15;
+    return subtotalWithDiscount + tax;
   }
 
   alertButtons = [
@@ -104,13 +128,12 @@ export class CheckoutPage implements OnInit {
       role: 'confirm',
       cssClass: 'alert-button-confirm',
       handler: () => {
-
         this.flightsService.buyTicket({
           departure_flight_id: this.departureFlight.flight_id,
           return_flight_id: this.returnFlight.flight_id,
           adults: this.adults,
           children: this.children,
-          total: this.getTotal()
+          total: this.getTotalWithDiscount()
         }).subscribe(data => {
           this.presentLoading();
         })
@@ -141,5 +164,15 @@ export class CheckoutPage implements OnInit {
     });
     this.router.navigate(['/mytickets'])
     await modal.present();
+  }
+
+  async showAlert(message: string) {
+    const alert = await this.alertController.create({
+      header: 'Invalid Discount Code',
+      message: message,
+      buttons: ['OK']
+    });
+
+    await alert.present();
   }
 }
